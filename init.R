e4ae809b03c4e7e8201c32912fd1a11d8f0171eb
rohit455273 <109446085+rohit455273@users.noreply.github.com>
2025-02-09 15:05:13 +0530

    Update init.R

--- init.R
+++ init.R
@@ -2,7 +2,8 @@
 #
 # Example R code to install packages if not already installed
 #
-my_packages = c("package_name_1", "package_name_2", ...)
+my_packages = c("glue","lubridate","shiny","glue","DBI","dplyr","jsonlite","httr","lubridate","waiter","shinyToastify","RSQLite"
+)
 install_if_missing = function(p) {
 if (p %in% rownames(installed.packages()) == FALSE) {
 install.packages(p)
